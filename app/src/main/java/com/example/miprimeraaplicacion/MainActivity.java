a9bd3068c62f48e9a7e20be30982f24e3e587deb
h4mil <Usss032024@ugb.edu.sv>
2025-04-22 10:28:21 -0600

    parcial2

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,60 +1,166 @@
 package com.example.miprimeraaplicacion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.FileProvider;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
     Button btn;
     TextView tempVal;
-    RadioGroup rgb;
-    RadioButton opt;
+    DB db;
+    String accion = "nuevo", idProducto = "";
+    ImageView img;
+    String urlCompletaFoto = "";
+    Intent tomarFotoIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = findViewById(R.id.imgFotoProducto);
+        db = new DB(this);
+        btn = findViewById(R.id.btnGuardarproducto);
+        btn.setOnClickListener(view->guardarProducto());
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
+        fab = findViewById(R.id.fabListaProductos);
+        fab.setOnClickListener(view->abrirVentana());
 
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
+        mostrarDatos();
+        tomarFoto();
+    }
+    private void mostrarDatos(){
+        try {
+            Bundle parametros = getIntent().getExtras();
+            accion = parametros.getString("accion");
+            if (accion.equals("modificar")) {
+                JSONObject datos = new JSONObject(parametros.getString("Producto"));
+                idProducto = datos.getString("idProducto");
 
-                double respuesta = 0.0;
+                tempVal = findViewById(R.id.txtCodigo);
+                tempVal.setText(datos.getString("codigo"));
 
-                opt = findViewById(R.id.optSuma);
-                if (opt.isChecked()) {
-                    respuesta = num1 + num2;
+                tempVal = findViewById(R.id.txtNombreProducto);
+                tempVal.setText(datos.getString("nombre"));
+
+                tempVal = findViewById(R.id.txtpresentacion);
+                tempVal.setText(datos.getString("presentacion"));
+
+                tempVal = findViewById(R.id.txtMarca);
+                tempVal.setText(datos.getString("marca"));
+
+                tempVal = findViewById(R.id.txtPrecio);
+                tempVal.setText(datos.getString("precio"));
+
+                urlCompletaFoto = datos.getString("urlFoto");
+                img.setImageURI(Uri.parse(urlCompletaFoto));
+            }
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
+        }
+    }
+    private void tomarFoto(){
+
+        img.setOnClickListener(view -> {
+            tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File fotoProducto = null;
+            try {
+                fotoProducto = crearImagenProducto();
+                if (fotoProducto != null) {
+                    Uri uriFotoProducto = FileProvider.getUriForFile(MainActivity.this,
+                            "com.ugb.miprimeraaplicacion.fileprovider", fotoProducto);
+                    tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoProducto);
+                    startActivityForResult(tomarFotoIntent, 1);
+                } else {
+                    mostrarMsg("No se pudo crear la imagen");
+
                 }
-                opt = findViewById(R.id.optResta);
-                if (opt.isChecked()) {
-                    respuesta = num1 - num2;
-                }
-                opt = findViewById(R.id.optMultiplicacion);
-                if (opt.isChecked()) {
-                    respuesta = num1 * num2;
-                }
-                opt = findViewById(R.id.optDivision);
-                if (opt.isChecked()) {
-                    respuesta = num1 / num2;
-                }
-                tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
+            }catch (Exception e){
+                mostrarMsg("Error: "+e.getMessage());
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if( requestCode==1 && resultCode==RESULT_OK ){
+                //Bitmap imagenBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageURI(Uri.parse(urlCompletaFoto));
+            }else{
+                mostrarMsg("No se tomo la foto.");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
+        }
+    }
+
+    private File crearImagenProducto() throws Exception {
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
+                fileName = "imagen_"+ fechaHoraMs+"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if( dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdir();
+        }
+        File image = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
+        urlCompletaFoto = image.getAbsolutePath();
+        return image;
+    }
+
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void abrirVentana(){
+        Intent intent = new Intent(this, lista_producto.class);
+        startActivity(intent);
+    }
+    private void guardarProducto() {
+        tempVal = findViewById(R.id.txtCodigo);
+        String codigo = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtNombreProducto);
+        String nombre = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtpresentacion);
+        String presentacion = tempVal.getText().toString();
+        tempVal = findViewById(R.id.txtMarca);
+        String marca = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtPrecio);
+        String precio = tempVal.getText().toString();
+
+
+
+
+        String[] datos = {idProducto, codigo, nombre, presentacion, marca, precio, urlCompletaFoto};
+
+        if (accion.equals("nuevo")) {
+            db.administrar_productos("agregar", datos);
+            Toast.makeText(getApplicationContext(), "Registro guardado con éxito.", Toast.LENGTH_LONG).show();
+        } else if (accion.equals("modificar")) {
+            db.administrar_productos("modificar", datos);
+            Toast.makeText(getApplicationContext(), "Registro actualizado con éxito.", Toast.LENGTH_LONG).show();
+        }
+        abrirVentana();
     }
 }
